05cf2373ceaaa4d83d14a55e44ba9df5b6a88374
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2025-01-18 14:02:48 +0530

    Blog Post App Dev Inprogress - Updated Static data with API data using Axios Get and Post methods

--- Redux_Learning/redux_app/src/features/posts/PostsList.js
+++ Redux_Learning/redux_app/src/features/posts/PostsList.js
@@ -1,31 +1,44 @@
 import React from "react";
-import { useSelector } from "react-redux";
-import PostAuthor from "./PostAuthor";
-import { selectAllPosts } from "./postsSlice";
-import TimeAgo from "./TimeAgo";
-import ReactionButtons from "./ReactionButtons";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  selectAllPosts,
+  getPostsStatus,
+  getPostsError,
+  fetchPosts,
+} from "./postsSlice";
+import { useEffect } from "react";
+import PostsExcerpts from "./PostsExcerpts";
 
 const PostsList = () => {
+  const dispatch = useDispatch();
   const posts = useSelector(selectAllPosts);
-  // The alternate way to show the lastest posts first
-  const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date));
-  const renderedPosts = orderedPosts.map((post) => (
-    <article key={post.id}>
-      <h3>{post.title}</h3>
-      <p>{post.content.substring(0, 100)}</p>
-      <p className="postCredit">
-        <PostAuthor userId={post.userId} />
-        <TimeAgo timestamp={post.date} />
-        <ReactionButtons post={post} />
-      </p>
-    </article>
-  ));
+  const postStatus = useSelector(getPostsStatus);
+  const error = useSelector(getPostsError);
+
+  useEffect(() => {
+    if (postStatus === "idle") {
+      dispatch(fetchPosts());
+    }
+  }, [postStatus, dispatch]);
+
+  let content;
+  if (postStatus === "loading") {
+    content = <p>"Loading..."</p>;
+  } else if (postStatus === "succeeded") {
+    // The alternate way to show the lastest posts first
+    const orderedPosts = posts
+      .slice()
+      .sort((a, b) => b.date.localeCompare(a.date));
+    content = orderedPosts.map((post) => <PostsExcerpts key={post.id} post={post}/>);
+  } else if(postStatus==='failed') {
+    content = <p>{error}</p>; 
+  }
   return (
     <section>
       <h2>Posts</h2>
       {/* Simple solution to show the latest posts first */}
       {/* {Array.from(renderedPosts).reverse()} */}
-      {renderedPosts}
+      {content}
     </section>
   );
 };
