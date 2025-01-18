185bb677db1283c5e8b96878c717c5d3889d2667
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2025-01-18 10:53:45 +0530

    Blog Post App Dev Inprogress - Time and Reactions Added.

--- Redux_Learning/redux_app/src/features/posts/PostsList.js
+++ Redux_Learning/redux_app/src/features/posts/PostsList.js
@@ -2,22 +2,29 @@ import React from "react";
 import { useSelector } from "react-redux";
 import PostAuthor from "./PostAuthor";
 import { selectAllPosts } from "./postsSlice";
+import TimeAgo from "./TimeAgo";
+import ReactionButtons from "./ReactionButtons";
 
 const PostsList = () => {
   const posts = useSelector(selectAllPosts);
-
-  const renderedPosts = posts.map((post) => (
+  // The alternate way to show the lastest posts first
+  const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date));
+  const renderedPosts = orderedPosts.map((post) => (
     <article key={post.id}>
       <h3>{post.title}</h3>
       <p>{post.content.substring(0, 100)}</p>
-      <p className="postCredit" >
+      <p className="postCredit">
         <PostAuthor userId={post.userId} />
+        <TimeAgo timestamp={post.date} />
+        <ReactionButtons post={post} />
       </p>
     </article>
   ));
   return (
     <section>
       <h2>Posts</h2>
+      {/* Simple solution to show the latest posts first */}
+      {/* {Array.from(renderedPosts).reverse()} */}
       {renderedPosts}
     </section>
   );
--- Redux_Learning/redux_app/src/features/posts/postsSlice.js
+++ Redux_Learning/redux_app/src/features/posts/postsSlice.js
@@ -1,40 +1,73 @@
-import {createSlice, nanoid} from "@reduxjs/toolkit";
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { sub } from "date-fns";
 
 const initialState = [
-    {
-        id: '1',
-        title: 'Learning Redux Toolkit',
-        content: "It's easy!"
-    },
-    {
-        id: '2',
-        title: 'Develop the App',
-        content: "It's awesome"
+  {
+    id: "1",
+    title: "Learning Redux Toolkit",
+    content: "It's easy!",
+    date: sub(new Date(), {minutes: 10}).toISOString(),
+    reactions: {
+        thumbsUp: 0,
+        wow: 0,
+        heart: 0,
+        rocket: 0,
+        coffee: 0
     }
-]
+  },
+  {
+    id: "2",
+    title: "Develop the App",
+    content: "It's awesome",
+    date: sub(new Date(), {minutes: 5}).toISOString(),
+    reactions: {
+        thumbsUp: 0,
+        wow: 0,
+        heart: 0,
+        rocket: 0,
+        coffee: 0
+    }
+
+  },
+];
 
 const postsSlice = createSlice({
-    name:'posts',
-    initialState,
-    reducers:{
-        postAdded:{ 
-            reducer(state, action) {
-            state.push(action.payload);
-        },
-        prepare(title, content, userId) {
-            return{
-                payload: {
-                    id: nanoid(),
-                    title,
-                    content,
-                    userId
-                }
+  name: "posts",
+  initialState,
+  reducers: {
+    postAdded: {
+      reducer(state, action) {
+        state.push(action.payload);
+      },
+      prepare(title, content, userId) {
+        return {
+          payload: {
+            id: nanoid(),
+            title,
+            content,
+            userId,
+            date: new Date().toISOString(),
+            reactions: {
+                thumbsUp: 0,
+                wow: 0,
+                heart: 0,
+                rocket: 0,
+                coffee: 0
             }
+          },
+        };
+      },
+    },
+    reactionAdded(state, action) {
+        const {postId, reaction} = action.payload;
+        const existingPost = state.find(post => post.id === postId);
+        if(existingPost){
+            existingPost.reactions[reaction]++;
         }
     }
-    }
-})
+  },
+});
 
 export const selectAllPosts = (state) => state.posts;
-export const {postAdded} = postsSlice.actions;
+export const { postAdded, reactionAdded } = postsSlice.actions;
 export default postsSlice.reducer;
